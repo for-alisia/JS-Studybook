fbdb98a930b8eeef4bee52c26fa1ac6cd5ccc2b0
for-alisia <for-alisia@yandex.ru>
2021-04-17 09:47:45 +0500

    Added zigzag conversion

--- problem_resolving/zigzag-conversion.js
+++ problem_resolving/zigzag-conversion.js
@@ -1,10 +1,33 @@
 var convert = function (s, numRows) {
-  let arr = new Array(numRows);
-  for (let i = 0; i < arr.length; i++) {
-    arr[i] = s[i];
+  if (numRows < 2) return s;
+
+  let result = Array.from({ length: numRows }, () => '');
+  let resStr = '';
+
+  let i = 0;
+
+  while (i !== s.length) {
+    let j = 0;
+
+    while (j !== numRows && i !== s.length) {
+      result[j] += s[i];
+      j++;
+      i++;
+    }
+
+    j = numRows - 2;
+    while (j !== 0 && i !== s.length) {
+      result[j] += s[i];
+      j--;
+      i++;
+    }
   }
 
-  console.log(arr);
+  for (let arr of result) {
+    resStr += arr;
+  }
+
+  return resStr;
 };
 
 console.log(convert('PAYPALISHIRING', 3));
